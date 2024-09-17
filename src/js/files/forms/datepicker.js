6b4310313533087d74e032a22a52b13b82b67b06
Pavlo-Goncharenko <82305213+Pavlo-Goncharenko@users.noreply.github.com>
2024-09-17 19:55:57 +0300

    Datepicker

--- src/js/files/forms/datepicker.js
+++ src/js/files/forms/datepicker.js
@@ -9,17 +9,21 @@ import datepicker from 'js-datepicker';
 
 if (document.querySelector('[data-datepicker]')) {
 	const picker = datepicker('[data-datepicker]', {
-		customDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
-		customMonths: ["Січ", "Лют", "Берез", "Квіт", "Трав", "Черв", "Лип", "Серп", "Верес", "Жовт", "Листоп", "Груд"],
-		overlayButton: 'Застосувати',
-		overlayPlaceholder: 'Рік (4 цифри)',
+		customDays: ["Mo", "Tu", "We", "Th", "Fri", "Sa", "Su"],
+		customMonths: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+		overlayButton: 'Apply',
+		overlayPlaceholder: 'Year (4 digits)',
 		startDay: 1,
 		formatter: (input, date, instance) => {
-			const value = date.toLocaleDateString()
-			input.value = value
+			// Форматирование только месяца и года
+			const value = date.toLocaleDateString('en', {
+				month: 'long', // Полное название месяца
+				year: 'numeric' // Число года
+			});
+			input.value = value;
 		},
 		onSelect: function (input, instance, date) {
-
+			// Можно добавить дополнительную логику при выборе даты
 		}
 	});
 	flsModules.datepicker = picker;
