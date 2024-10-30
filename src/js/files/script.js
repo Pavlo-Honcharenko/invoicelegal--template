c2974e895be6ce581738b2520737c462142d0c59
Pavlo-Goncharenko <82305213+Pavlo-Goncharenko@users.noreply.github.com>
2024-10-30 10:39:46 +0200

    bootstrap header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -7,19 +7,19 @@ import { flsModules } from "./modules.js";
 
 
 
-// Find the element with the class ".menu-item__more-links"
-const moreLinks = document.querySelector('.menu-item__more-links');
-// Check if the element exists
-if (moreLinks) {
-	// Add a click event listener to the element
-	moreLinks.addEventListener('click', function () {
-		// Find the closest parent element and toggle the "_open" class
-		const parent = this.closest('.menu-item'); // Replace with the actual parent class
-		if (parent) {
-			parent.classList.toggle('_open');
-		}
-	});
-}
+// Find the element with the class ".nav-item__more-links"
+// const moreLinks = document.querySelector('.nav-item__more-links');
+// // Check if the element exists
+// if (moreLinks) {
+// 	// Add a click event listener to the element
+// 	moreLinks.addEventListener('click', function () {
+// 		// Find the closest parent element and toggle the "_open" class
+// 		const parent = this.closest('.menu-item'); // Replace with the actual parent class
+// 		if (parent) {
+// 			parent.classList.toggle('_open');
+// 		}
+// 	});
+// }
 
 
 
